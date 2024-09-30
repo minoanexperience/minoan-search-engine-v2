70bb700c9b36a405b8213f5a49f81cfbba209f6a
aryachirag11 <aryachirag11@gmail.com>
2024-09-30 18:28:28 +0530

    code refactor

--- src/app/brand-section/brand-section.component.ts
+++ src/app/brand-section/brand-section.component.ts
@@ -1,21 +1,95 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {brandList} from "../constants";
 import {Brand, Product} from "../type";
+import {I18nPluralPipe, NgClass, NgIf} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SearchEngineService} from "../search-engine.service";
 
 @Component({
   selector: 'app-brand-section',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+    FormsModule,
+    NgClass,
+    I18nPluralPipe
+  ],
   templateUrl: './brand-section.component.html',
   styleUrl: './brand-section.component.scss'
 })
 export class BrandSectionComponent {
 
- @Input({required: true}) brandList: Brand[] = [];
 
-  constructor() {}
+  searchKeyword : string = '';
+  correctQuery: string = '';
+  loader: boolean = false;
+
+  constructor(public searchService : SearchEngineService) {}
+
+  /**
+   * Event fired when you press Enter in search box
+   * @param event
+   * @param element
+   */
+  onEnterKeyInSearch(event: any, element: HTMLInputElement): void {
+    if (event.keyCode === 13) {
+      this.onSearchProducts();
+      element.blur();
+    }
+  }
+
+  /**
+   * clear event for search text
+   * @param event
+   */
+  onSearchInputChange(event: any) {
+    if (event == '') {
+      // this.queryParams.search = undefined
+      this.removeFilter();
+    }
+  }
+
+  /**
+   *
+   */
+  removeFilter(){
+    this.searchKeyword = '';
+  }
+
 
   openBrandDetailsModal(brand : any){
     window.open(brand?.webLink)
+  }
+
+
+  /**
+   * search product based on query
+   * @param query
+   */
+  onSearchProducts() {
+    if (!this.loader) {
+      this.searchService.updateLoader(true);
+      this.searchService.updateLoaderText('Hang tight, we are fetching products for you....')
+      const requestBody = {query : this.searchKeyword};
+      this.loader = true;
+      this.searchService.getSearchedProducts(requestBody).subscribe({
+        next: (response: any) => {
+          this.searchService.resultProductList = response.results;
+          this.searchService.resultBrandList = response?.brands ? response.brandList : brandList;
+          this.correctQuery = response?.corrected_query;
+          this.loader = false;
+          this.searchService.updateLoader(false);
+          this.searchService.updateLoaderText('');
+
+        },
+        error: (err:any) => {
+          this.loader = false;
+          console.log(err);
+          this.searchService.updateLoader(false);
+          this.searchService.updateLoaderText('');
+        }
+      })
+    }
   }
 }
--- src/app/products-section/products-section.component.ts
+++ src/app/products-section/products-section.component.ts
@@ -1,33 +1,45 @@
 import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {Product} from "../type";
 import {NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
-import {SlicePipe} from "@angular/common";
+import {NgIf, SlicePipe} from "@angular/common";
+import {SearchEngineService} from "../search-engine.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-products-section',
   standalone: true,
-  imports: [NgbPaginationModule, SlicePipe],
+  imports: [NgbPaginationModule, SlicePipe, NgIf],
   templateUrl: './products-section.component.html',
   styleUrl: './products-section.component.scss'
 })
-export class ProductsSectionComponent implements OnChanges{
+export class ProductsSectionComponent{
   @ViewChild('scrollContainer') scrollContainer!: ElementRef;
-  @Input({required : true}) productsList!: Product[];
+  productsList: Product[] = [];
 
   //pagination data
   page: number = 1;
   limit: number = 25;
   skip = 0;
-  productListLength: number = this.productsList?.length;
+  productListLength: number = 0;
+
+  productListSubscription: Subscription;
 
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['productsList']) {
-      this.productListLength = this.productsList?.length;
-    }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   if (changes['productsList']) {
+  //     this.productListLength = this.productsList?.length;
+  //   }
+  // }
+
+  constructor(public searchService : SearchEngineService) {
+    this.productListSubscription = this.searchService.RESULT_PRODUCT_LIST.subscribe({
+      next: (value: any) => {
+        this.productsList = value;
+        this.productListLength = this.productsList?.length;
+      }
+    })
   }
-
-  constructor() {}
 
   protected readonly PLACEHOLDER_URL = 'assets/images/coming_soon.png';
 
