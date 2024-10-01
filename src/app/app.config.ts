cc138d2ed022308af054e1162d61f96babcee534
Dipit Maurya <dipitmaurya@gmail.com>
2024-10-01 19:53:02 +0530

    Changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideClientHydration} from "@angular/platform-browser";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+  ]
 };
