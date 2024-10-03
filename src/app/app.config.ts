f04e4886e34a66b3575afc3b80be5650bc1bad2b
chirag-minoan <chirag@minoanexperience.com>
2024-10-03 17:28:07 +0530

    [fix] : first demo fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withFetch} from "@angular/common/http";
 import {provideClientHydration} from "@angular/platform-browser";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideClientHydration(),
+    provideAnimations(),
   ]
 };
